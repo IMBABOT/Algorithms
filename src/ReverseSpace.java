6be7e9977b7d0d145a642079f7ddfc7f48d804df
IMBABOT <zlovred12@gmail.com>
2020-09-03 18:38:25 +0300

    reverseWithSpace

---
+++ src/ReverseSpace.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ReverseSpace {
+
+    public int[] reverseWithSpace(int[]arr){
+        int[] result = new int[arr.length];
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < arr.length && end >=0){
+            result[end] = arr[start];
+            start++;
+            end--;
+        }
+
+
+        return result;
+    }
+}
