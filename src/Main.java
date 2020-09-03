ac17f14743192a1cfd45b01add6f6268b314e724
IMBABOT <zlovred12@gmail.com>
2020-09-03 18:06:54 +0300

    temp

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
 
-        Reverse rv = new Reverse();
-
-        System.out.println(Arrays.toString(rv.reverse(arr)));
     }
 }
