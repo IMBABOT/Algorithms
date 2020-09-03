94f30d7e96d730f9cd74d8ec0a8637e322bd5ade
IMBABOT <zlovred12@gmail.com>
2020-09-03 18:26:13 +0300

    reverse

---
+++ src/Reverse.java
@@ -0,0 +1,18 @@
+public class Reverse {
+
+    public int[] reverse(int[]arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return arr;
+    }
+}
