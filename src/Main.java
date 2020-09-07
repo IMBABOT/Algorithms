0a1c0a37c32c5feaffa02eb8323d9b6f1aa61613
unknown <zlovred12@gmail.com>
2020-09-07 14:01:27 +0300

    checkBalance

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        ShiftElements se = new ShiftElements();
-        int[]arr = {1,2,3,4,5,6,7,8,9,10};
+        CheckBalance cb = new CheckBalance();
 
-        se.shiftElements(arr, 3);
-
-        System.out.println(Arrays.toString(arr));
-
+        System.out.println(cb.checkBalance(new int[]{1, 1, 1, 2, 1}));
     }
 }
