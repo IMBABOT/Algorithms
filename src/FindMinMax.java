9c12c1508caafaafd3c4cd6abcd665374d24d134
unknown <zlovred12@gmail.com>
2020-09-07 14:13:55 +0300

    findMinMax

---
+++ src/FindMinMax.java
@@ -0,0 +1,22 @@
+public class FindMinMax {
+
+    public int findMin(int[] arr){
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (min > arr[i]) min = arr[i];
+        }
+
+        return min;
+    }
+
+    public int findMax(int[] arr){
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (max < arr[i]) max = arr[i];
+        }
+
+        return max;
+    }
+}
