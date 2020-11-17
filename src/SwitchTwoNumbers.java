32ec4fe806434dcc9863839228202f293fe5d71c
unknown <zlovred12@gmail.com>
2020-11-17 20:51:06 +0300

    number

---
+++ src/SwitchTwoNumbers.java
@@ -0,0 +1,47 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+public class SwitchTwoNumbers {
+
+    private static int number;
+    private Scanner scanner;
+    private int tries;
+    private int variable = 1;
+    private String temp;
+
+    public SwitchTwoNumbers(int tries) {
+        this.scanner = new Scanner(System.in);
+        this.tries = tries;
+
+    }
+
+    public void switchTwoNumbers(String letter){
+        System.out.println("Enter q");
+        while (tries > 0){
+            if (scanner.nextLine().equals("q")) {
+                variable = variable == 1 ? 2 : 1;
+            }
+            System.out.println(variable);
+            tries--;
+        }
+    }
+
+
+    public void switchTwoNumbersWithIf(String letter){
+        System.out.println("Enter q");
+        while (tries > 0){
+            if (scanner.nextLine().equals("q")) {
+                if(variable == 1) {
+                    variable = 2;
+                } else {
+                    variable = 1;
+                }
+            }
+            System.out.println(variable);
+            tries--;
+        }
+    }
+
+}
+
