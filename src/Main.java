4601339c9f06c3aeee6bb50348845da29708162e
unknown <zlovred12@gmail.com>
2020-12-30 20:57:45 +0300

    insertElement

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[10];
+        int[] arr = new int[11];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             arr[i] = i;
         }
 
+
+        InsertElement.insertElement(arr, 8, 6);
+
         System.out.println(Arrays.toString(arr));
 
-       MoveElementToFirstPlace.moveElementToFirstPlace(arr, 2);
-
-        System.out.println(Arrays.toString(arr));
     }
 }
