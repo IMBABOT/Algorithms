30910da26f4e276362dfedfeb8c5e2b6f28298aa
unknown <zlovred12@gmail.com>
2020-12-31 18:27:02 +0300

    LinkedList

--- src/DataStructures/LinkedList/LinkedList.java
+++ src/DataStructures/LinkedList/LinkedList.java
@@ -1,16 +1,64 @@
 package DataStructures.LinkedList;
 
-public class LinkedList {
+import java.util.Map;
+
+public class LinkedList<T> {
     private static class Node<E>{
         E element;
         Node<E> next;
-        Node<E> prev;
 
-        public Node(E element, Node<E> next, Node<E> prev){
+        public Node(E element){
             this.element = element;
-            this.next = next;
-            this.prev = prev;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof Node)) return false;
+            Node node = (Node) obj;
+            return  (node.equals(obj));
+        }
+
+        @Override
+        public String toString() {
+            return element.toString();
         }
     }
 
+
+    private Node head;
+
+    public LinkedList(){
+        head = null;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void insert(T t){
+        Node<T> node = new Node<>(t);
+        node.next = head;
+        head = node;
+    }
+
+    public T remove(){
+        if (isEmpty()) return null;
+        Node<T> remove = head;
+        head = head.next;
+        return (T) remove;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return null;
+        Node<T> current = head;
+        StringBuilder sb = new StringBuilder("[");
+        while (current != null){
+            sb.append(current);
+            current = current.next;
+            sb.append((current == null) ? "]" : ", ");
+        }
+
+        return sb.toString();
+    }
 }
--- src/DataStructures/Stack/Main.java
+++ src/DataStructures/Stack/Main.java
@@ -1,16 +1,11 @@
 package DataStructures.Stack;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Stack stack = new Stack(5);
-
-        stack.push(1);
-        System.out.println(stack.pop());
-
-        stack.reverse();
 
 
 
