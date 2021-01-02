b17eb208745561b46e7a8bddf5444ead46e35d99
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-02 17:22:13 +0300

    DoublyLinkedList

--- src/DataStructures/LinkedList/DoublyLinkedList.java
+++ src/DataStructures/LinkedList/DoublyLinkedList.java
@@ -2,13 +2,22 @@ package DataStructures.LinkedList;
 
 public class DoublyLinkedList<E> {
 
-    private static class Node<E> {
+    private static class Node<E>{
         E element;
         Node<E> next;
         Node<E> prev;
 
-        public Node(E element) {
+
+        public Node(E element, Node<E> next, Node<E> prev){
             this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+
+        @Override
+        public String toString() {
+            return element.toString();
         }
 
         @Override
@@ -16,52 +25,43 @@ public class DoublyLinkedList<E> {
             if (this == obj) return true;
             if (!(obj instanceof Node)) return false;
             Node node = (Node) obj;
-            return (node.equals(obj));
-        }
-
-        @Override
-        public String toString() {
-            return element.toString();
+            return node.equals(obj);
         }
     }
 
-    private Node head;
+    int size;
+    Node<E> first;
+    Node<E> last;
 
-    public int getSize() {
-        return size;
-    }
-
-    private int size;
-
-    public DoublyLinkedList() {
-        head = null;
+    public DoublyLinkedList(){
         size = 0;
+
     }
 
-    public void insert(E element){
-        Node<E> node = new Node<>(element);
-        Node<E> temp = head;
-        node.prev = null;
-        node.next = null;
-        if (head == null){
-            head = temp = node;
-        }else{
-            temp.next = node;
-            node.prev = temp;
-            temp = node;
-            size++;
-        }
+    public void insertInBegin(E element){
+        Node<E> temp = first;
+        Node<E> newNode = new Node<>(element, first, null);
+        first = newNode;
+        if (first == null)
+            last = newNode;
+        else
+            first.prev = newNode;
+        size++;
     }
+
+
 
     public boolean isEmpty(){
         return size == 0;
     }
 
+
     @Override
     public String toString() {
         if (isEmpty()) return null;
-        Node<E> current = head;
+        Node<E> current = first;
         StringBuilder sb = new StringBuilder("[");
+
         while (current != null){
             sb.append(current);
             current = current.next;
--- src/DataStructures/LinkedList/Main.java
+++ src/DataStructures/LinkedList/Main.java
@@ -1,10 +1,13 @@
 package DataStructures.LinkedList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-
+        List<Integer> list = new LinkedList<>();
 
         doublyLinkedList.insert(10);
         doublyLinkedList.insert(20);
