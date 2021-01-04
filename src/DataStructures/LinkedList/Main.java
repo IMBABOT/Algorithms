8f46a883b882a3e37765295c414ce291b2647147
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-04 12:17:40 +0300

    insertAtBegin

--- src/DataStructures/LinkedList/Main.java
+++ src/DataStructures/LinkedList/Main.java
@@ -1,31 +1,18 @@
 package DataStructures.LinkedList;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-        List<Integer> list = new LinkedList<>();
+        CircularLinkedList<Integer> circularLinkedList = new CircularLinkedList<>();
 
-        doublyLinkedList.insertInEnd(10);
-        doublyLinkedList.insertInEnd(20);
-        doublyLinkedList.insertInEnd(30);
-        doublyLinkedList.insertInEnd(40);
-        doublyLinkedList.insertInEnd(50);
-        doublyLinkedList.insertInBegin(10);
-        doublyLinkedList.insertInBegin(60);
-        doublyLinkedList.insertInPosition(80, 3);
-        doublyLinkedList.insertInPosition(90, 7);
-        doublyLinkedList.deleteFromPosition(5);
+        circularLinkedList.insertAtBegin(1);
+        circularLinkedList.insertAtBegin(2);
+        circularLinkedList.insertAtBegin(3);
+        circularLinkedList.insertAtBegin(4);
 
-        System.out.println(doublyLinkedList.size);
-
-        System.out.println(doublyLinkedList);
-        doublyLinkedList.reverse();
-        System.out.println(doublyLinkedList);
+        System.out.println(circularLinkedList);
 
     }
 }
