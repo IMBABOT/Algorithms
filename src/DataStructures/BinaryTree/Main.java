f47efcfcca1978da1a3878fa591753a667d6de2d
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-07 18:56:01 +0300

    preorderpostorderinorder

--- src/DataStructures/BinaryTree/Main.java
+++ src/DataStructures/BinaryTree/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         RecursiveBinaryTree tree = new RecursiveBinaryTree();
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(8);
+        tree.insert(10);
         tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
-        tree.insert(-1);
-        tree.insert(-3);
-        //tree.inOrderDisplay();
-        //tree.displayPreorder();
-        tree.displayPostorder();
+
+
+
+        tree.postOrder();
     }
 }
--- src/DataStructures/BinaryTree/RecursiveBinaryTree.java
+++ src/DataStructures/BinaryTree/RecursiveBinaryTree.java
@@ -2,69 +2,86 @@ package DataStructures.BinaryTree;
 
 public class RecursiveBinaryTree {
 
-    private static class Node{
-        public int element;
-        public Node left;
-        public Node right;
-        public Node(int element){
+    private static class Node {
+        private int element;
+        private Node left;
+        private Node right;
+
+        private Node(int element) {
             this.element = element;
         }
     }
 
     private Node root;
 
-    public RecursiveBinaryTree(){
+
+    public RecursiveBinaryTree() {
         root = null;
     }
 
-    public void insert(int element){
+    public void insert(int element) {
         root = insertNode(root, new Node(element));
     }
 
     private Node insertNode(Node currentparent, Node node) {
         if (currentparent == null) {
             return node;
-        }else if (node.element > currentparent.element) {
-             currentparent.left = insertNode(currentparent.left, node);
-        }else if (node.element < currentparent.element) {
-             currentparent.right = insertNode(currentparent.right, node);
+        } else if (node.element > currentparent.element) {
+            currentparent.left = insertNode(currentparent.left, node);
+        } else if (node.element < currentparent.element) {
+            currentparent.right = insertNode(currentparent.right, node);
         }
         return currentparent;
     }
 
 
-
-    public void inOrderDisplay(){
-        traversalLeft(root.left);
-        System.out.println(root.element);
-        traversalRight(root.right);
+    public void preOrder() {
+        traversalPreOrder(root);
     }
 
-    private void traversalLeft(Node node) {
-        if (node != null) {
+    public void inOrder() {
+        traversalInOrder(root);
+    }
+
+    public void postOrder() {
+        traversalPostOrder(root);
+    }
+
+
+    private void traversalPreOrder(Node node) {
+        if (node == null) {
+            return;
+        } else {
             System.out.println(node.element);
-            traversalLeft(node.left);
+            traversalInOrder(node.left);
+            traversalInOrder(node.right);
         }
     }
-    private void traversalRight(Node node){
-        if (node != null){
+
+    private void traversalInOrder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            traversalInOrder(node.left);
             System.out.println(node.element);
-            traversalRight(node.right);
+            traversalInOrder(node.right);
         }
     }
 
-    public void displayPreorder(){
-        System.out.println(root.element);
-        traversalLeft(root.left);
-        traversalRight(root.right);
-    }
 
-    public void displayPostorder(){
-        traversalLeft(root.left);
-        traversalRight(root.right);
-        System.out.println(root.element);
+    private void traversalPostOrder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            traversalInOrder(node.left);
+            traversalInOrder(node.right);
+            System.out.println(node.element);
+        }
     }
-
 }
+
+
+
+
 
 
