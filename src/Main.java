744bc7b2e64b39c7fc12d2a8ecaff83cc90ee5be
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-10 00:13:44 +0300

    insertionSort

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import Sorting.UpdateBubbleSort;
+import Sorting.InsertionSort;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{16, 14, 5, 6, 8};
-        System.out.println(Arrays.toString(UpdateBubbleSort.updateBubbleSort(arr)));
+        int[] arr = new int[]{8, 9, 10, 11, 3, 2, -35, -85, 85, 112};
+        System.out.println(InsertionSort.insertionSort(arr));
+        System.out.println(Arrays.toString(arr));
 
     }
 
