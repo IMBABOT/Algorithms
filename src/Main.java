3d384aaa4eb262da8ae5830ee9f4e9735d38bf7e
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-09 19:29:31 +0300

    linearSearch

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
-import Sorting.QuickSort;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        QuickSort quickSort = new QuickSort();
-        int[] arr = new int[]{7,6,10,5,9,2,1,15,7};
-        quickSort.quickSort(arr, 0, arr.length-1);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = new int[]{1, 8, 10, 12, 16, 35, 66, 77, 30};
+        //System.out.println(LinearSearch.linearSearch(arr, 11));
+        System.out.println(LinearSearch.findIndex(arr, 1));
 
     }
 
