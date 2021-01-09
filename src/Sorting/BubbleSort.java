86ec66499d0d23d1f5e7b53d2e5305efb2864ca7
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-09 22:12:34 +0300

    update

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -3,12 +3,12 @@ package Sorting;
 public class BubbleSort<T extends Comparable<? super T>> {
         //O(n*n)
         public  T[] bubbleSort(T[] arr) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                for (int j = i + 1; j < arr.length; j++) {
-                    if (arr[i].compareTo(arr[j]) > 0) {
-                        T temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
+            for (int i = 0; i < arr.length-1; i++) {
+                for (int j = 0; j < arr.length - 1 - i; j++) {
+                    if (arr[j].compareTo(arr[j + 1]) > 0) {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
