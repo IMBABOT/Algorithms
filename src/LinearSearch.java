88aeaabe5408ab47d79b130158730c06f929c768
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-09 19:29:54 +0300

    linearSearch

---
+++ src/LinearSearch.java
@@ -0,0 +1,29 @@
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int element){
+        int result = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == element){
+                result = arr[i];
+            }else {
+                throw new RuntimeException("No such element: " + element);
+            }
+        }
+        return result;
+    }
+
+    public static int findIndex(int[] arr, int index){
+        int i = 0;
+        int result = 0;
+
+        while (i < arr[i]){
+            if (i == index){
+                result = arr[i];
+                break;
+            }
+            i++;
+        }
+
+        return result;
+    }
+}
