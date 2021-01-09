61bd416bfebde1e3a4e043b5202a02b23ee5f9f6
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-09 14:59:19 +0300

    quickSort

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         QuickSort quickSort = new QuickSort();
         int[] arr = new int[]{7,6,10,5,9,2,1,15,7};
         quickSort.quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
 
     }
 
--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -6,47 +6,33 @@ public class QuickSort {
 
 
     public void quickSort(int[]arr, int start, int end){
-        int position = 0;
         if (start < end) {
-           position = partition(arr, start, end);
+            int position = partition(arr, start, end);
+
+            quickSort(arr, start, position - 1);
+            quickSort(arr, position, end);
         }
-     //   quickSort(arr, start, position - 1);
-      //  quickSort(arr, position + 1, end);
     }
 
     private int partition(int[] arr, int start, int end){
-        start = 0;
-        end = arr.length - 1;
-        int pivot = arr[start];
+        int pivot = arr[(start + end) / 2];
 
-        System.out.println(Arrays.toString(arr));
-        while (start < end) {
-            while (arr[start] <= pivot) {
+        while (start <= end) {
+            while (arr[start] < pivot) {
                 start++;
             }
             while (arr[end] > pivot) {
                 end--;
             }
-            if (start > end){
-                int temp = arr[end];
-                arr[end] = arr[0];
-                arr[0] = temp;
-                break;
+            if (start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
             }
-
-           swap(arr, start, end);
-
         }
-
-        System.out.println(Arrays.toString(arr));
-
-        return arr[end];
-
-    }
-
-    private void swap(int arr[], int first, int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        return start;
     }
 }
