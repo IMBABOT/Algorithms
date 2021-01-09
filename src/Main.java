af3b09b2307572a0aab7bb9a4386b7fb886ad1f2
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-09 22:33:07 +0300

    UpdateBubbleSort

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import Sorting.BubbleSort;
+import Sorting.UpdateBubbleSort;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Integer[] arr = new Integer[]{3, -1, 10, 5, 8, 12, 34, 21, 12, 11, 10};
-        BubbleSort<Integer> sort = new BubbleSort<>();
-        sort.bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = new int[]{16, 14, 5, 6, 8};
+        System.out.println(Arrays.toString(UpdateBubbleSort.updateBubbleSort(arr)));
 
     }
 
