b62d51fa12c14c58552612c31c66f8ace711e19c
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-10 14:30:24 +0300

    selectionSort

--- src/DataStructures/Stack/Main.java
+++ src/DataStructures/Stack/Main.java
@@ -1,8 +1,13 @@
 package DataStructures.Stack;
+
+import Sorting.SelectionSort;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(SelectionSort.selectionSort(new int[]{8,3,11,10,12,166,0,2,1,3})));
     }
 }
 
